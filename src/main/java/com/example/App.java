e2acc9366d54e7d726a3b47bbb28851ffcaa84d8
Igor <igorponticelli8@gmail.com>
2023-10-18 18:20:40 +0300

    fazendoQuestao#1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Conjuntos" );
+
+        Set<Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+        s1.add(null);
+
+        System.out.println(s1);
+        System.out.println("Conjuntio s1 contem 1?");
+        System.out.println(s1.contains(1));
+
+        Set<Integer> empty = new HashSet<>();
+        empty.add(3);
+        
+        System.out.println(s1.containsAll(empty));
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import java.util.*;
 
 /**
  * Unit test for simple App.
@@ -15,6 +17,14 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        Set<Integer> s1 = new HashSet<>();
+        s1.add(1);
+        s1.add(2);
+        s1.add(3);
+        s1.add(null);
+
+        String actual = s1.toString();
+        String expected = "[null, 1, 2, 3]";
+        assertEquals( expected, actual );
     }
 }
